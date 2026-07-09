25e6a4bc3ac7fa333a59d1282f809938c30d4584
Krzysztof <kpasek22@gmail.com>
2026-07-09 18:37:20 +0200

    init commit

---
+++ backend/src/main/kotlin/com/infraView/InfraViewApplication.kt
@@ -0,0 +1,11 @@
+package com.infraView
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfraViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<InfraViewApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/infraView/InfraViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.infraView
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InfraViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
